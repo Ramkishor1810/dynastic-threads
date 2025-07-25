609998374829e593d99b242985c7f7c55d78c2bb
Ramkishor1810 <ramkishor181004@gmail.com>
2025-07-25 06:48:41 +0530

    Create Biggest.java

---
+++ Biggest.java
@@ -0,0 +1,21 @@
+class Biggest
+{
+public static void main(String args[])
+{
+int n1='5';
+int n2='4';
+int n3='7';
+
+if((n1>n2)&&(n1>n3))
+{
+System.out.println(n1+"  is biggest");
+}
+else if((n2>n1)&&(n2>n3))
+{
+System.out.println(n2+"  is biggest");
+}
+else
+{System.out.println(n3+" is biggest");
+}
+}
+}
